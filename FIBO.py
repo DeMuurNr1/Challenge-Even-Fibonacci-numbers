92df867b42d7d657e51c6d10d8f54220faf2bafe
DeMuurNr1 <83447961+DeMuurNr1@users.noreply.github.com>
2023-11-27 14:46:20 +0100

    Add files via upload

---
+++ FIBO.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def FIBO():
+    T     =   0
+    X     =   0
+    Y     =   1
+    
+    while X <= 4e6:
+        #print(X)        #print fibonacci numbers
+        if X%2==0:
+            T = T + X
+        X, Y =  Y, X + Y
+    
+    print("T = ", T)    #sumation of all even numbers
+    #print("X = ", X)
+    #print("Y = ", Y)
+    
+FIBO()
